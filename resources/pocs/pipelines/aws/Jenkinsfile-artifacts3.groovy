f135f62a1e8bc486ff40335cb5ca9bbaaf268a6c
carlosrodlop <it.carlosrodlop@gmail.com>
2020-03-26 18:52:29 +0100

    simplifying aws s3 publisher

--- resources/pocs/pipelines/aws/Jenkinsfile-artifacts3.groovy
+++ resources/pocs/pipelines/aws/Jenkinsfile-artifacts3.groovy
@@ -1,23 +1,20 @@
 // resources/pocs/pipelines/aws/Jenkinsfile-artifacts3.groovy
-def props 
 pipeline {
     agent {
         kubernetes {
           containerTemplate {
-            name 'aws-cli'
-            image 'carlosrodlop/jenkins-slave-aws:e7c3999'
+            name 'slave'
+            image 'jenkins/slave:alpine'
             ttyEnabled true
             }
         }
     }
     stages {
-        stage('archiving into AWS s3') {
+        stage('archiving artifacts into AWS s3') {
             steps {
                 script {
-                    props = readJSON text: '{ "DSE": "crodriguezlopez", "Title": "Lead" }', returnPojo: true
-                    sh "touch empty.txt"
+                    sh "dd if=/dev/urandom of=artifact.txt bs=5MB count=1"
                 }
-                writeJSON file: "output.json", json: props, pretty: 4
                 archiveArtifacts artifacts: "*.*", fingerprint: true
             }
         }
