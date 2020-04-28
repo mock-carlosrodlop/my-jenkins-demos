807fe24f0aa85c133632f446bf8f9c59eae42bc7
carlosrodlop <it.carlosrodlop@gmail.com>
2020-04-28 13:30:25 +0200

    sync with the cloudbees-oss/pipeline-home-demo

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
-# My Pipeline Home
+# Pipeline Home Demo
 
-This repo contains resources for Post in the CloudBees Blog: [Template Catalog: Welcome to the Pipeline as Code family](https://www.cloudbees.com/blog)
+This repo is linked to the post `Template Catalog: Welcome to the Pipeline as Code family` from the [CloudBees Blog](https://www.cloudbees.com/blog).
+
+![fusion image](resources/img/fusion.png)
+
+Not only the [Template Catalog](https://docs.cloudbees.com/docs/admin-resources/latest/pipeline-templates-user-guide/setting-up-a-pipeline-template-catalog) and [Shared Libraries](https://jenkins.io/doc/book/pipeline/shared-libraries/) are complementary to each other but also compatible in their code structures. That’s why they can be integrated into a single point of control which it was named in the post as the "The Pipeline Home".
+
+## Demo: Maven Docker App run by Kubernetes Cloud Agents
+
+This demo presents a template that model the pipeline Continous Integration process in the Acme Corporation.  The pipeline defines Maven Apps of the corporation than are deployed as Docker containers. It integrates CloudBees Core and GitHub Enterprise via webhooks. Builds are managed by Kubernetes agents. Finally, Successful builds create an image with the project artifacts and it gets uploaded to Dockerhub (Docker-in-Docker).
--- vars/mavenK8Project.groovy
+++ vars/mavenK8Project.groovy
@@ -4,76 +4,92 @@ def call(configYaml) {
     Map config = readYaml text: "${configYaml}"
 
     K8_AGENT_YAML = "${config.k8_agent_yaml}"
-    GITHUB_BRANCH = "${config.gh_branch}"
-    // See https://github.com/jenkinsci/git-plugin#environment-variables
     git_commit = ""
     git_currentBranch = ""
     git_repo = ""
 
     pipeline {
-        agent {
-            kubernetes {
-                defaultContainer "git-maven"
-                yaml libraryResource("agents/k8s/${K8_AGENT_YAML}")
-            }
-        }
+        agent none
         options {
-            buildDiscarder(logRotator(numToKeepStr: '5', artifactNumToKeepStr: '5'))
-        }
-        environment {
-            DOCKERFILE_PATH = "${config.dockerfile_path}"
+            buildDiscarder(logRotator(numToKeepStr: "5", artifactNumToKeepStr: "5"))
         }
         stages {
-            stage("Print configuration") {
-                steps {
-                    writeYaml file: "config.yaml", data: config  
-                    sh "cat config.yaml"
-                }
-            }
-            stage("Checkout") {
-                environment {
-                    GITHUB_CREDENTIALS = "${config.gh_cred}"
-                    GITHUB_REPO = "${config.gh_repo}"
+            stage ("Skip Run?") {     
+                when {
+                    anyOf { changeset "**/template.yaml"; changeset "**/Jenkinsfile"}
                 }
                 steps {
-                    container(name: "git-maven"){
-                        script {
-                            if (GITHUB_BRANCH == "") {
-                                echo "Pipeline Multibranch detected"
-                                git credentialsId: "${GITHUB_CREDENTIALS}" , url: "${GITHUB_REPO}"
-                            } else {
-                                echo "Pipeline non Multibranch detected"
-                                git branch: "${GITHUB_BRANCH}", credentialsId: "${GITHUB_CREDENTIALS}" , url: "${GITHUB_REPO}"
-                            }
-                            git_commit = "${GIT_COMMIT}"
-                            git_currentBranch = "${GIT_LOCAL_BRANCH}"
-                            git_repo = sh(script: "basename '${GITHUB_REPO}' .git", returnStdout: true).trim()
-                        }
+                    echo "Aborting Pipeline due to changes are coming from Template definition"
+                    script{
+                        currentBuild.result = "ABORTED"
                     }
                 }
             }
-            stage("Build") {
-                steps {
-                    sh "mvn clean package -Dmaven.test.skip=true"
-                    archiveArtifacts artifacts: "config.yaml, target/*.jar", fingerprint: true
-                    stash name: "docker", includes: "config.yaml, target/*.jar, ${DOCKERFILE_PATH}"
+            stage ("Run") {
+                when {
+                    beforeAgent true
+                    expression { currentBuild.result != "ABORTED" }
                 }
-            }
-            stage("Test") {
-                steps {
-                    sh 'mvn clean test'
-                    junit allowEmptyResults: true, testResults: 'target/surefire-reports/*.xml'
+                agent {
+                    kubernetes {
+                        namespace "cje-support-agents"
+                        defaultContainer "maven"
+                        yaml libraryResource("agents/k8s/java/${K8_AGENT_YAML}.yaml")
+                    }
                 }
-            }
-            stage("Publish in Registry") {
                 environment {
-                    DOCKER_DESTINATION = "${config.docker_registry}/${git_repo}_${git_currentBranch}:${git_commit}"
+                    DOCKERFILE_PATH = "${config.d_path.trim()}"
                 }
-                steps {
-                    container(name: "kaniko", shell: "/busybox/sh") {
-                        dir("to_build") {
-                            unstash "docker"
-                            sh "/kaniko/executor --dockerfile `pwd`/${DOCKERFILE_PATH} --context `pwd` --destination ${DOCKER_DESTINATION}"
+                stages {
+                    stage("Print configuration") {
+                        steps {
+                            writeYaml file: "config.yaml", data: config 
+                            sh "cat config.yaml"
+                        }
+                    }
+                    stage("Checkout") {
+                        environment {
+                            DOCKER_IMAGE_LATEST = "${config.d_latest}"
+                        }
+                        steps {
+                            // Checkout step is done implicitly being a multibranch pipeline template
+                            script {
+                                git_currentBranch = "${BRANCH_NAME}"
+                                if (DOCKER_IMAGE_LATEST == "false") {
+                                    echo "Tagging image with commit"
+                                    git_commit = sh(script: "git rev-parse --short=5 ${GIT_COMMIT}", returnStdout: true).trim()
+                                } else {
+                                    echo "Tagging image as latest"
+                                    git_commit = "latest"
+                                }
+                            }
+                        }
+                    }
+                    stage("Build") {
+                        steps {
+                            sh "mvn clean package -Dmaven.test.skip=true"
+                            archiveArtifacts artifacts: "config.yaml, target/*.jar", fingerprint: true
+                            stash name: "docker", includes: "config.yaml, target/*.jar, ${DOCKERFILE_PATH}"
+                        }
+                    }
+                    stage("Test") {
+                        steps {
+                            sh "mvn clean test"
+                            junit allowEmptyResults: true, testResults: "target/surefire-reports/*.xml"
+                        }
+                    }
+                    stage("Publish in Registry") {
+                        environment {
+                            GIT_PARAM_REPO = "${config.ghe_repo}"
+                            DOCKER_DESTINATION = "${config.d_registry.trim()}/${GIT_PARAM_REPO}_${git_currentBranch}:${git_commit}"
+                        }
+                        steps {
+                            container(name: "kaniko", shell: "/busybox/sh") {
+                                dir ("unstash"){ // To avoid java.nio.file.AccessDeniedException ... example-app.jar
+                                    unstash "docker"
+                                    sh "/kaniko/executor --dockerfile `pwd`/${DOCKERFILE_PATH} --context `pwd` --destination ${DOCKER_DESTINATION}"
+                                }
+                            }
                         }
                     }
                 }
