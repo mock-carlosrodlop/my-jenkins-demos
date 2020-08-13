70a13b9c4a880b6f94887be8f141976c62906297
carlosrodlop <it.carlosrodlop@gmail.com>
2020-08-13 12:14:46 +0200

    Untracked files issue resolved to fix .gitignore

--- resources/cli/jenkins-cli.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-JENKINS_USER_ID=my_user
-JENKINS_API_TOKEN=*****
-JENKINS_URL=https://jenkins.example/
-
-java -jar jenkins-cli.jar -auth $JENKINS_USER_ID:$JENKINS_API_TOKEN -s $JENKINS_URL "$@"
