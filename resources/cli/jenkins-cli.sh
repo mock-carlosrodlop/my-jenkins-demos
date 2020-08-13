0c8065e4a2c21377e56bdbc0129903a3d1b172df
carlosrodlop <it.carlosrodlop@gmail.com>
2020-08-13 11:52:23 +0200

    preparing para plugin catalog cli

--- resources/cli/jenkins-cli.sh
+++ resources/cli/jenkins-cli.sh
@@ -2,6 +2,6 @@
 
 JENKINS_USER_ID=carlos
 JENKINS_API_TOKEN=*****
-JENKINS_URL=https://core.gcp.support.beescloud.com/cjoc/
+JENKINS_URL=https://jenkins.example/
 
 java -jar jenkins-cli.jar -auth $JENKINS_USER_ID:$JENKINS_API_TOKEN -s $JENKINS_URL "$@"
