6d62e4b7fbed0617f0f5ae6a9577c4a8f1ece2ef
carlosrodlop <it.carlosrodlop@gmail.com>
2021-03-03 14:19:39 +0100

    preparing simulation

--- resources/pipelines/sharedLibraryClassScripted/Jenkinsfile-carlos.groovy
+++ resources/pipelines/sharedLibraryClassScripted/Jenkinsfile-carlos.groovy
@@ -2,11 +2,11 @@
 
 @Library(["my-test-lib@master"]) _
 
-import lib.dummy.HelloWorld
+import lib.dummy.HelloWorld_v1
 
 def g 
 
 node {
-    g = new HelloWorld()
+    g = new HelloWorld_v1()
     g.greating ("Hola")
 }
