f1aabe3aaf86bc040ab4a9cb18f79b0f8eb7634b
carlosrodlop <it.carlosrodlop@gmail.com>
2021-03-03 15:42:37 +0100

    environment variable

--- src/lib/dummy/HelloWorld_v1.groovy
+++ src/lib/dummy/HelloWorld_v1.groovy
@@ -3,10 +3,12 @@
 // Ref:https://www.jenkins.io/doc/book/pipeline/shared-libraries/#accessing-steps
 package lib.dummy
 
-def greating (msg) {  
+def msg
+def greating (input) {
+    msg = "${input}"
     stage("Hello") {
-        println( "${msg}")
-        sh ('echo \"${msg}\"')      
+        println("${msg}")
+        sh ('echo \"${msg}\"') 
     }
 }
 
